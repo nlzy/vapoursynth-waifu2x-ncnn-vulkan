6dee2ea1c34b7c34c734230e9171760ab384350b
NaLan ZeYu <nalanzeyu@gmail.com>
2021-06-09 12:39:42 +0800

    update waifu2x.cpp

--- src/waifu2x.cpp
+++ src/waifu2x.cpp
@@ -28,7 +28,7 @@
 #include "waifu2x.hpp"
 
 #define DIV_CEIL(a, b) (((a) + (b) - 1) / (b))
-#define ALIGN_CEIL(a, b) (((a) + (b) - 1) / (b) * (b))
+#define PAD_TO_ALIGN(a, b) ((((a) + (b) - 1) / (b)) * (b) - (a))
 
 static const uint32_t waifu2x_preproc_fp32_spv_data[] = {
     #include "waifu2x_preproc_fp32.spv.hex.h"
@@ -61,14 +61,14 @@ Waifu2x::Waifu2x(int width, int height, int scale, int tilesize, int gpuid, int 
 
     std::vector<ncnn::vk_specialization_type> specializations;
     waifu2x_preproc = new ncnn::Pipeline(net.vulkan_device());
-    waifu2x_preproc->set_optimal_local_size_xyz(32, 32, 3);
+    waifu2x_preproc->set_optimal_local_size_xyz(8, 8, 3);
     if (net.opt.use_fp16_storage)
         waifu2x_preproc->create(waifu2x_preproc_fp16_spv_data, sizeof(waifu2x_preproc_fp16_spv_data), specializations);
     else
         waifu2x_preproc->create(waifu2x_preproc_fp32_spv_data, sizeof(waifu2x_preproc_fp32_spv_data), specializations);
 
     waifu2x_postproc = new ncnn::Pipeline(net.vulkan_device());
-    waifu2x_postproc->set_optimal_local_size_xyz(32, 32, 3);
+    waifu2x_postproc->set_optimal_local_size_xyz(8, 8, 3);
     if (net.opt.use_fp16_storage)
         waifu2x_postproc->create(waifu2x_postproc_fp16_spv_data, sizeof(waifu2x_postproc_fp16_spv_data), specializations);
     else
@@ -94,93 +94,91 @@ int Waifu2x::process(const float *srcR, const float *srcG, const float *srcB,
 
     const int xtiles = DIV_CEIL(width, tilesize);
     const int ytiles = DIV_CEIL(height, tilesize);
-    const int prepadding_right = prepadding + ALIGN_CEIL(width, 4 / scale) - width;
-    const int prepadding_bottom = prepadding + ALIGN_CEIL(height, 4 / scale) - height;
 
     for (int yi = 0; yi < ytiles; yi++) {
         ncnn::VkCompute cmd(net.vulkan_device());
 
+        const int tile_nopad_y0 = yi * tilesize;
+        const int tile_nopad_y1 = std::min(tile_nopad_y0 + tilesize, height);
+        const int tile_nopad_h = tile_nopad_y1 - tile_nopad_y0;
+        const int prepadding_bottom = prepadding + PAD_TO_ALIGN(tile_nopad_h, 4 / scale);
+        const int tile_pad_y0 = std::max(tile_nopad_y0 - prepadding, 0);
+        const int tile_pad_y1 = std::min(tile_nopad_y1 + prepadding_bottom, height);
+        const int tile_pad_h = tile_pad_y1 - tile_pad_y0;
+
+
         // upload
+        ncnn::Mat in(width, tile_pad_h, RGB_CHANNELS, sizeof(float));
+        for (int y = 0; y < tile_pad_h; y++) {
+            memcpy((float*)in.channel(0) + y * width, srcR + (y + tile_pad_y0) * srcStride, sizeof(float) * width);
+            memcpy((float*)in.channel(1) + y * width, srcG + (y + tile_pad_y0) * srcStride, sizeof(float) * width);
+            memcpy((float*)in.channel(2) + y * width, srcB + (y + tile_pad_y0) * srcStride, sizeof(float) * width);
+        }
+        
         ncnn::VkMat in_gpu;
-        {
-            const int in_tile_y0 = std::max(yi * tilesize - prepadding, 0);
-            const int in_tile_y1 = std::min((yi + 1) * tilesize + (yi == ytiles - 1 ? prepadding_bottom : prepadding), height);
-            const int in_tile_w = width;
-            const int in_tile_h = in_tile_y1 - in_tile_y0;
-
-            ncnn::Mat tmp;
-            tmp.create(in_tile_w, in_tile_h, RGB_CHANNELS, sizeof(float));
-            float* in_tile_r = tmp.channel(0);
-            float* in_tile_g = tmp.channel(1);
-            float* in_tile_b = tmp.channel(2);
-            const float* sr = srcR + in_tile_y0 * srcStride;
-            const float* sg = srcG + in_tile_y0 * srcStride;
-            const float* sb = srcB + in_tile_y0 * srcStride;
-            for (int y = 0; y < in_tile_h; y++) {
-                memcpy(in_tile_r + in_tile_w * y, sr + srcStride * y, sizeof(float) * in_tile_w);
-                memcpy(in_tile_g + in_tile_w * y, sg + srcStride * y, sizeof(float) * in_tile_w);
-                memcpy(in_tile_b + in_tile_w * y, sb + srcStride * y, sizeof(float) * in_tile_w);
+        cmd.record_clone(in, in_gpu, opt);
+        if (xtiles > 1) {
+            if (cmd.submit_and_wait()) {
+                return ERROR_UPLOAD;
             }
-
-            cmd.record_clone(tmp, in_gpu, opt);
-
-            if (xtiles > 1) {
-                if (cmd.submit_and_wait()) {
-                    return ERROR_UPLOAD;
-                }
-                cmd.reset();
-            }
+            cmd.reset();
         }
 
-        const int out_tile_y0 = std::max(yi * tilesize, 0);
-        const int out_tile_y1 = std::min((yi + 1) * tilesize, height);
+
         ncnn::VkMat out_gpu;
-        out_gpu.create(width * scale, (out_tile_y1 - out_tile_y0) * scale, RGB_CHANNELS, sizeof(float), blob_vkallocator);
+        out_gpu.create(width * scale, tile_nopad_h * scale, RGB_CHANNELS, sizeof(float), blob_vkallocator);
+
         for (int xi = 0; xi < xtiles; xi++) {
+            const int tile_nopad_x0 = xi * tilesize;
+            const int tile_nopad_x1 = std::min(tile_nopad_x0 + tilesize, width);
+            const int tile_nopad_w = tile_nopad_x1 - tile_nopad_x0;
+            const int prepadding_right = prepadding + PAD_TO_ALIGN(tile_nopad_w, 4 / scale);
+
+            ncnn::VkMat in_tile_gpu(tile_nopad_x1 - tile_nopad_x0 + prepadding + prepadding_right,
+                                    tile_nopad_y1 - tile_nopad_y0 + prepadding + prepadding_bottom,
+                                    RGB_CHANNELS, net.opt.use_fp16_storage ? 2u : 4u, 1, blob_vkallocator);
+
             // preproc
-            ncnn::VkMat in_tile_gpu;
             {
-                // crop tile
-                int tile_x0 = xi * tilesize;
-                int tile_x1 = std::min((xi + 1) * tilesize, width) + prepadding + (xi == xtiles - 1 ? prepadding_right : prepadding);
-                int tile_y0 = yi * tilesize;
-                int tile_y1 = std::min((yi + 1) * tilesize, height) + prepadding + (yi == ytiles - 1 ? prepadding_bottom : prepadding);
-
-                in_tile_gpu.create(tile_x1 - tile_x0, tile_y1 - tile_y0, RGB_CHANNELS, sizeof(float), 1, blob_vkallocator);
-
                 std::vector<ncnn::VkMat> bindings(2);
                 bindings[0] = in_gpu;
                 bindings[1] = in_tile_gpu;
 
-                std::vector<ncnn::vk_constant_type> constants(9);
+                std::vector<ncnn::vk_constant_type> constants(10);
                 constants[0].i = in_gpu.w;
                 constants[1].i = in_gpu.h;
                 constants[2].i = in_gpu.cstep;
                 constants[3].i = in_tile_gpu.w;
                 constants[4].i = in_tile_gpu.h;
                 constants[5].i = in_tile_gpu.cstep;
-                constants[6].i = std::max(prepadding - yi * tilesize, 0);
+                constants[6].i = prepadding;
                 constants[7].i = prepadding;
-                constants[8].i = xi * tilesize;
+                constants[8].i = tile_nopad_x0;
+                constants[9].i = std::min(tile_nopad_y0, prepadding);
 
-                cmd.record_pipeline(waifu2x_preproc, bindings, constants, in_tile_gpu);
+                ncnn::VkMat dispatcher;
+                dispatcher.w = in_tile_gpu.w;
+                dispatcher.h = in_tile_gpu.h;
+                dispatcher.c = RGB_CHANNELS;
+
+                cmd.record_pipeline(waifu2x_preproc, bindings, constants, dispatcher);
             }
+
 
             // waifu2x
             ncnn::VkMat out_tile_gpu;
-            {
-                ncnn::Extractor ex = net.create_extractor();
 
-                ex.set_blob_vkallocator(blob_vkallocator);
-                ex.set_workspace_vkallocator(blob_vkallocator);
-                ex.set_staging_vkallocator(staging_vkallocator);
+            ncnn::Extractor ex = net.create_extractor();
+            ex.set_blob_vkallocator(blob_vkallocator);
+            ex.set_workspace_vkallocator(blob_vkallocator);
+            ex.set_staging_vkallocator(staging_vkallocator);
 
-                ex.input("Input1", in_tile_gpu);
+            ex.input("Input1", in_tile_gpu);
 
-                if (ex.extract("Eltwise4", out_tile_gpu, cmd)) {
-                    return ERROR_EXTRACTOR;
-                }
+            if (ex.extract("Eltwise4", out_tile_gpu, cmd)) {
+                return ERROR_EXTRACTOR;
             }
+            
 
             // postproc
             {
@@ -195,16 +193,17 @@ int Waifu2x::process(const float *srcR, const float *srcG, const float *srcB,
                 constants[3].i = out_gpu.w;
                 constants[4].i = out_gpu.h;
                 constants[5].i = out_gpu.cstep;
-                constants[6].i = xi * tilesize * scale;
-                constants[7].i = out_gpu.w - xi * tilesize * scale;
+                constants[6].i = tile_nopad_x0 * scale;
+                constants[7].i = std::min(out_gpu.w - tile_nopad_x0 * scale, tilesize * scale);
 
                 ncnn::VkMat dispatcher;
-                dispatcher.w = out_gpu.w - xi * tilesize * scale;
+                dispatcher.w = std::min(out_gpu.w - tile_nopad_x0 * scale, tilesize * scale);
                 dispatcher.h = out_gpu.h;
-                dispatcher.c = 3;
+                dispatcher.c = RGB_CHANNELS;
 
                 cmd.record_pipeline(waifu2x_postproc, bindings, constants, dispatcher);
             }
+            
 
             if (xtiles > 1) {
                 if (cmd.submit_and_wait()) {
@@ -222,16 +221,10 @@ int Waifu2x::process(const float *srcR, const float *srcG, const float *srcB,
                 return ERROR_DOWNLOAD;
             }
 
-            const float* out_tile_r = out.channel(0);
-            const float* out_tile_g = out.channel(1);
-            const float* out_tile_b = out.channel(2);
-            float* dr = dstR + yi * tilesize * scale * dstStride;
-            float* dg = dstG + yi * tilesize * scale * dstStride;
-            float* db = dstB + yi * tilesize * scale * dstStride;
             for (int y = 0; y < out.h; y++) {
-                memcpy(dr + dstStride * y, out_tile_r + out.w * y, out.w * sizeof(float));
-                memcpy(dg + dstStride * y, out_tile_g + out.w * y, out.w * sizeof(float));
-                memcpy(db + dstStride * y, out_tile_b + out.w * y, out.w * sizeof(float));
+                memcpy(dstR + tile_nopad_y0 * scale * dstStride + y * dstStride, (float *)out.channel(0) + y * out.w, out.w * sizeof(float));
+                memcpy(dstG + tile_nopad_y0 * scale * dstStride + y * dstStride, (float *)out.channel(1) + y * out.w, out.w * sizeof(float));
+                memcpy(dstB + tile_nopad_y0 * scale * dstStride + y * dstStride, (float *)out.channel(2) + y * out.w, out.w * sizeof(float));
             }
         }
     }
